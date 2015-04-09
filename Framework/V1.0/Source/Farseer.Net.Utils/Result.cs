b0a430a2a61e896a5167cc6b7cb1ea6bcdd87f6d
steden <1470804@qq.com>
2015-04-09 14:32:47 +0800

    添加Result工具类

---
+++ Framework/V1.0/Source/Farseer.Net.Utils/Result.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace FS.Utils
+{
+    /// <summary>
+    /// 返回结果情况
+    /// </summary>
+    public class Result
+    {
+        public Result()
+        {
+            LstErrMsg = new List<string>();
+        }
+
+        /// <summary>
+        /// 是否出错
+        /// </summary>
+        public bool IsError { get { return LstErrMsg.Count > 0; } }
+
+        /// <summary>
+        /// 出错信息
+        /// </summary>
+        public List<string> LstErrMsg { get; set; }
+
+        /// <summary>
+        /// 出错代码
+        /// </summary>
+        public int ErrCode { get; set; }
+
+        /// <summary>
+        /// 添加出错消息
+        /// </summary>
+        /// <param name="dicError">出错消息</param>
+        public void Add(Dictionary<string, List<string>> dicError)
+        {
+            foreach (var keyValue in dicError) { LstErrMsg.AddRange(keyValue.Value); }
+        }
+
+        /// <summary>
+        /// 添加出错消息
+        /// </summary>
+        /// <param name="err">出错消息</param>
+        /// <param name="format">Format格式化</param>
+        public void Add(string err, params object[] format)
+        {
+            LstErrMsg.Add(string.Format(err, format));
+        }
+
+        /// <summary>
+        /// 添加出错消息
+        /// </summary>
+        /// <param name="err">出错消息</param>
+        /// <param name="format">Format格式化</param>
+        /// <param name="isTrue">条件为真是，添加错误消息</param>
+        public void Add(bool isTrue, string err, params object[] format)
+        {
+            if (isTrue) { Add(err, format); }
+        }
+
+        /// <summary>
+        /// 清除错误消息
+        /// </summary>
+        public void Reset()
+        {
+            ErrCode = 0;
+            LstErrMsg.Clear();
+        }
+    }
+}
