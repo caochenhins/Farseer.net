2262de16acb2451329a58e706228c7bdb97568ed
steden <1470804@qq.com>
2015-04-24 18:06:51 +0800

    增加Socket发送立即获得消息的方法

--- Framework/V1.0/Source/Farseer.Net.Utils/WinSocket/ClientSocket.cs
+++ Framework/V1.0/Source/Farseer.Net.Utils/WinSocket/ClientSocket.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace FS.Utils.WinSocket
 {
@@ -107,6 +108,72 @@ namespace FS.Utils.WinSocket
         public void Send(byte[] buffer, int bufferSize = 1024, Action<StateObject> sendCallBack = null)
         {
             _socket.BeginSend(buffer, 0, buffer.Length, 0, SendCallback, new StateObject(bufferSize, _socket, sendCallBack));
+        }
+
+        /// <summary>
+        /// 发送后，马上返回接收的信息
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port">端口</param>
+        /// <param name="msg"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="sendCallBack">发送成功之后的回调</param>
+        public static string SendToReceive(string ip, int port, string msg, int bufferSize = 1024, Action<StateObject> sendCallBack = null, AddressFamily addressFamily = AddressFamily.InterNetwork, SocketType socketType = SocketType.Stream, ProtocolType protocolType = ProtocolType.Tcp)
+        {
+            using (var socket = new ClientSocket(ip, port, addressFamily, socketType, protocolType))
+            {
+                socket.Start();
+                return socket.SendToReceive(msg, bufferSize);
+            }
+        }
+
+        /// <summary>
+        /// 发送后，马上返回接收的信息
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port">端口</param>
+        /// <param name="msg"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="sendCallBack">发送成功之后的回调</param>
+        public static byte[] SendToReceive(string ip, int port, byte[] msg, int bufferSize = 1024, Action<StateObject> sendCallBack = null, AddressFamily addressFamily = AddressFamily.InterNetwork, SocketType socketType = SocketType.Stream, ProtocolType protocolType = ProtocolType.Tcp)
+        {
+            using (var socket = new ClientSocket(ip, port, addressFamily, socketType, protocolType))
+            {
+                socket.Start();
+                return socket.SendToReceive(msg, bufferSize);
+            }
+        }
+
+        /// <summary>
+        /// 发送后，马上返回接收的信息
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="sendCallBack">发送成功之后的回调</param>
+        public byte[] SendToReceive(byte[] msg, int bufferSize = 1024, Action<StateObject> sendCallBack = null)
+        {
+            byte[] result = null;
+            this.Init(o => { result = o.Buffer; });
+            this.Send(msg, bufferSize, sendCallBack);
+            var resetCount = 0;
+            while (result == null && ++resetCount < 30) { Thread.Sleep(100); }
+            return result;
+        }
+
+        /// <summary>
+        /// 发送后，马上返回接收的信息
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="sendCallBack">发送成功之后的回调</param>
+        public string SendToReceive(string msg, int bufferSize = 1024, Action<StateObject> sendCallBack = null)
+        {
+            var result = string.Empty;
+            this.Init(o => { result = o.Msg; });
+            this.Send(msg, bufferSize, sendCallBack);
+            var resetCount = 0;
+            while (string.IsNullOrWhiteSpace(result) && ++resetCount < 30) { Thread.Sleep(100); }
+            return result;
         }
 
         /// <summary>
